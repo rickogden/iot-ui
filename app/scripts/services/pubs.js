466944d56bc8d556961b6c762aded898ef0dfcf8
Rick Ogden <rick@rickogden.com>
2014-03-27 22:35:46 +0000

    migrating API to Beanstalk.

--- app/scripts/services/pubs.js
+++ app/scripts/services/pubs.js
@@ -2,7 +2,7 @@
 
 angular.module('iotUiApp')
     .service('Pubs', ['$resource', function Pubs($resource) {
-        return $resource('http://ricklab.net/iot/pubs', {limit: 20}, {
+        return $resource('http://iot-api-env-25nf533imt.elasticbeanstalk.com/pubs', {limit: 20}, {
             query: {method: 'GET', isArray: true}
         });
     }]);
